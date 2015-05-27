5f085596884c4a1f505af073cf754118a98d6c07
Bernard Notarianni <bernard.notarianni@gmail.com>
2015-05-27 17:59:51 +0200

    Faster tdd loop

--- clients/haskell/tdd.sh
+++ clients/haskell/tdd.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+DIR=/tmp/carpaccio
+mkdir $DIR
+
 while true; do
 
-    cabal test --test-option=--color
+    ghc src/Model.hs test/* -odir $DIR -hidir $DIR -o $DIR/carpaccio_test
+    $DIR/carpaccio_test
     echo ""
 
     inotifywait -qr -e modify -e create -e move -e delete src test --exclude "\.\#.*"
